050b18451b83dbf2a3d75efffd9b76b6a6c0a516
Bernardo Estrada <a01704320@tec.com>
2022-03-01 02:15:44 +0000

    Deploy website - based on fd86384a321b9d3640cfdb7db4cdb776508a953c

---
+++ assets/js/814f3328.9ce8d065.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmm=self.webpackChunkmm||[]).push([[535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Minutas Mutate","permalink":"/blog/welcome"}]}')}}]);
